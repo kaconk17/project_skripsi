1a78f3e332fac645de698367858cf89d693755a4
kaconk <harutonouciha@gmail.com>
2020-11-14 21:43:46 +0700

    live predict

--- mainMenu.py
+++ mainMenu.py
@@ -11,6 +11,8 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QFileDialog
 from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtCore import QTimer, QDateTime
+from PyQt5.uic import loadUi
 import cv2
 import sys
 import numpy as np
@@ -18,11 +20,26 @@ from tensorflow.keras.models import load_model
 from keras.preprocessing import image
 
 
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(864, 600)
+class Ui_MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Ui_MainWindow, self).__init__()
+        loadUi("gui/mainMenu.ui", self)
+        #MainWindow.setObjectName("MainWindow")
+        #MainWindow.resize(864, 600)
         self.filename = None
+        self.image = None
+        self.liveStat = False
+
+        self.labelScreen.setStyleSheet("background-color: black")
+        self.btnLive.clicked.connect(self.goLive)
+        self.btnOpen.clicked.connect(self.loadImage)
+        self.model = load_model('models/material.h5')
+        self.checkPredict.stateChanged.connect(self.checkstate)
+        self.btnPredict.clicked.connect(self.singleImage)
+        self.checkPredict.setEnabled(False)
+        self.btnPredict.setEnabled(False)
+
+        """
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
@@ -56,7 +73,6 @@ class Ui_MainWindow(object):
         self.horizontalLayout_2.addWidget(self.btnLive)
         self.btnOpen = QtWidgets.QPushButton(self.centralwidget)
         self.btnOpen.setObjectName("btnOpen")
-        self.btnOpen.clicked.connect(self.loadImage)
         self.horizontalLayout_2.addWidget(self.btnOpen)
         self.verticalLayout.addLayout(self.horizontalLayout_2)
         self.horizontalLayout.addLayout(self.verticalLayout)
@@ -78,9 +94,9 @@ class Ui_MainWindow(object):
         self.menuMenu.addAction(self.actionOpen_File)
         self.menuMenu.addAction(self.actionClose)
         self.menubar.addAction(self.menuMenu.menuAction())
-        self.model = load_model('models/material.h5')
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        """
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
@@ -95,13 +111,50 @@ class Ui_MainWindow(object):
         self.actionOpen_File.setText(_translate("MainWindow", "Open File"))
         self.actionClose.setText(_translate("MainWindow", "Close"))
 
+    def goLive(self):
+        if self.liveStat:
+            self.stopLive()
+        else:
+            self.liveCam()
+
+    def stopLive(self):
+        if self.liveStat:
+            self.vtimer.stop()
+            self.capture.release()
+            #self.labelScreen.setStyleSheet("background-color: black")
+            self.labelScreen.clear()
+            self.liveStat = False
+            self.btnLive.setText("LIVE")
+            self.checkPredict.setEnabled(False)
+            self.btnPredict.setEnabled(False)
+
+    def liveCam(self):
+        self.liveStat = True
+        self.btnLive.setText("STOP LIVE")
+        self.checkPredict.setEnabled(True)
+        self.btnPredict.setEnabled(True)
+        cameraName = "0"
+        if len(cameraName) == 1:
+        	self.capture = cv2.VideoCapture(int(cameraName))
+        else:
+        	self.capture = cv2.VideoCapture(cameraName)
+        self.vtimer = QTimer(self)
+        self.vtimer.timeout.connect(self.updateFrame)
+        self.vtimer.start(40)
+
+    def updateFrame(self):
+        ret, self.image = self.capture.read()
+        if self.checkPredict.isChecked() == True:
+            self.tampilGambar(self.image, True)
+        else:
+            self.tampilGambar(self.image, False)
+
     def loadImage(self):
         self.filename = QFileDialog.getOpenFileName(filter="Image (*.*)")[0]
         self.image = cv2.imread(self.filename)
-        self.tampilGambar(self.image)
+        self.tampilGambar(self.image, True)
 
-    def tampilGambar(self, original):
-        thasil = self.prediksi(original)
+    def tampilGambar(self, original, pred):
         gambar = cv2.resize(original, (400, 400))
         qformat = QImage.Format_Indexed8
         if len(gambar.shape) == 3:
@@ -113,7 +166,11 @@ class Ui_MainWindow(object):
         outImage = outImage.rgbSwapped()
         self.labelScreen.setPixmap(QPixmap.fromImage(outImage))
         self.labelScreen.setScaledContents(True)
-        self.labelMaterial.setText(thasil)
+        if pred:
+            thasil = self.prediksi(original)
+            self.labelMaterial.setText(thasil)
+        
+            
 
     def prediksi(self,gambar):
         img = cv2.resize(gambar, (400,400))
@@ -130,12 +187,25 @@ class Ui_MainWindow(object):
         print(hasil)
         return hasil
 
+    def singleImage(self):
+        #self.vtimer.stop()
+        self.stopLive()
+        self.tampilGambar(self.image, True)
+
+
+    def checkstate(self):
+        if self.checkPredict.isChecked() == True:
+            self.btnPredict.setEnabled(False)
+        else:
+            self.btnPredict.setEnabled(True)
+
 
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
+    #MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
+    #ui.setupUi(MainWindow)
+    #MainWindow.show()
+    ui.show()
     sys.exit(app.exec_())
